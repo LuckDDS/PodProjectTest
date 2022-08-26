b81bafd82229de1218a68f78f6ffc0703e9e92c9
dongdeshuai <dongdeshuai@tal.com>
2022-08-26 13:59:42 +0800

    添加类

---
+++ PodProjectTest/Classes/PodProjectTest.h
@@ -0,0 +1,12 @@
+//
+//  PodProjectTest.h
+//  PodProjectTest
+//
+//  Created by hwl on 2022/8/26.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodProjectTest : NSObject
++ (NSInteger)sumA:(NSInteger)a sunB:(NSInteger)b;
+@end
---
+++ PodProjectTest/Classes/PodProjectTest.m
@@ -0,0 +1,14 @@
+//
+//  PodProjectTest.m
+//  PodProjectTest
+//
+//  Created by hwl on 2022/8/26.
+//
+
+#import "PodProjectTest.h"
+
+@implementation PodProjectTest
++ (NSInteger)sumA:(NSInteger)a sunB:(NSInteger)b {
+    return a + b;
+}
+@end
